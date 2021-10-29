210f78ff28785218e89d42679c6db10dce8e0afb
leo011619 <leonard.ferraz@hotmail.com>
2021-10-29 17:16:53 -0300

    Criação do script funcional que faz os números da tabuada aparecerem

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Olá");
+function gerar() {
+  var num = document.getElementById("txtn");
+  var tabu = document.getElementById("txttabu");
+
+  if (num.value.length == 0) {
+    window.alert("[Erro] Por favor coloque um número.");
+  } else {
+    var n = Number(num.value);
+    tabu.innerHTML = ``;
+    for (i = 1; i <= 10; i++) {
+      var c = n * i;
+      tabu.innerHTML += ` <br> ${n} * ${i} = ${c} </br>`;
+    }
+  }
+}
